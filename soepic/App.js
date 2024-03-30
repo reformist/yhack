ed8ac2b538fe3ffd01c305ac1f8619e965984724
kevingutierrez04 <kevingut04@hotmail.com>
2024-03-30 16:14:04 -0400

    basic camera functonality

--- soepic/App.js
+++ soepic/App.js
@@ -1,11 +1,44 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-
+import { StyleSheet, Text, View, Button} from 'react-native';
+import { Camera } from 'expo-camera';
+import React, { useState, useEffect, useRef} from 'react';
 export default function App() {
-  return (
+
+  const [hasPermission, setHasPermission] = useState(null);
+  const cameraRef = useRef(null);
+  const [photoUri, setPhotoUri] = useState(null);
+  const[text, setText] = useState('daddy');
+  
+  useEffect(() => {
+    (async () => {
+      const { status } = await Camera.requestCameraPermissionsAsync();
+      setHasPermission(status === 'granted');
+    })();
+
+  }, []);
+  const takePicture = async () => {
+    if (cameraRef.current) {
+      const photo = await cameraRef.current.takePictureAsync();
+      let photoUri = photo.uri;
+       setText(photoUri);
+    }
+  };
+
+  //TODO: this if block is a little funky, figure out hasPermission
+  if (hasPermission === 'granted') {
+    //return <View />; doesnt do shit? 
+    <Text> i love men</Text>  //this isn't displayed
+  }
+  if (hasPermission === false) {
+    return <Text>No access to camera</Text>;
+  }
+
+  return (//cam style ... type = {type}
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <Camera style={styles.camera} ref ={cameraRef}></Camera>
+    <Button title="Take Picture" onPress={takePicture} />
+    <StatusBar style="auto" />
+    <Text> {text} </Text>
     </View>
   );
 }
@@ -17,4 +50,8 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  camera: {
+    flex: 0.75,
+    width: '100%',
+  }
 });
