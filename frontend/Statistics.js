baa4b42928424ed1c01b6b4ea34c8d1eba651895
paeb37 <btp2109@columbia.edu>
2024-03-31 10:05:44 -0400

    update

--- frontend/Statistics.js
+++ frontend/Statistics.js
@@ -4,8 +4,6 @@ import { LinearGradient } from 'expo-linear-gradient';
 import {useNavigation } from '@react-navigation/native';
 
 
-
-
 const NewPage = () => {
     const navigation = useNavigation();
 
@@ -40,47 +38,4 @@ const styles = StyleSheet.create({
       }
   });
   
-  export default NewPage;
-import React from 'react';
-import {View, Text, StyleSheet, Button} from 'react-native';
-import { LinearGradient } from 'expo-linear-gradient';
-import {useNavigation } from '@react-navigation/native';
-
-
-
-
-const NewPage = () => {
-    const navigation = useNavigation();
-
-    return (
-        
-        <View style = {StyleSheet.container}>
-            <LinearGradient 
-                colors={['#6BFFB8', '#2CEAA3', '#28965A']}
-                start = { { x: 0.1, y: 0.15}}
-                style={styles.background}
-                />
-            <Text> Statistics </Text>
-            <Button title="Go to Shopping List" onPress={() => navigation.navigate('ShoppingList')} />
-
-        </View>
-    )
-}
-
-const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-      justifyContent: 'center',
-      alignItems: 'center',
-    },
-
-    background : {
-        position: 'absolute',
-        left: 0,
-        right: 0,
-        top: 0,
-        height: 1000,
-      }
-  });
-  
-  export default NewPage;
+export default NewPage;
